0fae123e4c6947b46588d284f91aa2e8f2ec1895
Jordan <jordanonuma@outlook.com>
2019-04-30 05:55:22 -1000

    Added frame for struct Zombie {}.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -3,4 +3,8 @@ pragma solidity ^0.4.25;
 contract ZombieFactory {
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+
+    } //end struct Zombie{}
 } //end contract ZombieFactory {}
