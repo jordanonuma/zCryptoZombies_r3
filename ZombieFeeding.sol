4aa7ea7eba3d6ee30ef033e3d4bba0644b5b7847
Jordan <jordanonuma@outlook.com>
2019-05-14 05:54:56 -1000

    Declared struct 'myZombie' using array zombies[].

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -5,6 +5,9 @@ contract ZombieFeeding is ZombieFactory {
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
+    //Declares a local Zombie[] named 'myZombie' (which will be a storage pointer) and
+    //sets this variable to be equal to index _zombieId in our zombies[] array.
+    Zombie storage myZombie = zombies[_zombieId];
   } //end function feedAndMultiply()
 
 } //end contract ZombieFeeding {}
