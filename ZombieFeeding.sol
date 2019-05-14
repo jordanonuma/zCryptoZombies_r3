e7a568da6c079bf91df12ad7d5e973a192077fc2
Jordan <jordanonuma@outlook.com>
2019-05-14 05:46:43 -1000

    Added frame for feedAndMultiply().

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -3,5 +3,9 @@ import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+    
+  } //end function feedAndMultiply()
+
 } //end contract ZombieFeeding {}
 
