891545d497a4ec8d3320bdf333f3b068bc5071a1
Jordan <jordanonuma@outlook.com>
2019-05-15 05:47:24 -1000

    Truncated DNA to only 16 digits.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -8,6 +8,7 @@ contract ZombieFeeding is ZombieFactory {
     //Declares a local Zombie[] named 'myZombie' (which will be a storage pointer) and
     //sets this variable to be equal to index _zombieId in our zombies[] array.
     Zombie storage myZombie = zombies[_zombieId];
+    _targetDna = _targetDna % dnaModulus;   
   } //end function feedAndMultiply()
 
 } //end contract ZombieFeeding {}
