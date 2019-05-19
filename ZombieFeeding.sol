767c2c5cb061a780da42f9e7bd850fa607444fe8
Jordan <jordanonuma@outlook.com>
2019-05-19 08:27:50 -1000

    Added frame for function that zombies use to attack the CryptoKitty.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -30,5 +30,9 @@ contract ZombieFeeding is ZombieFactory {
     _createZombie("NoName", newDna);
   } //end function feedAndMultiply()
 
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    
+  } //end function feedOnKitty()
+
 } //end contract ZombieFeeding {}
 
