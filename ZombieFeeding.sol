bb37cc3dc46dda4946fb4487bc8d96220d0d16bd
Jordan <jordanonuma@outlook.com>
2019-05-19 08:33:01 -1000

    Read in the kitty's DNA.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -32,6 +32,7 @@ contract ZombieFeeding is ZombieFactory {
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
     uint kittyDna;
+    (,,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
   } //end function feedOnKitty()
 
 } //end contract ZombieFeeding {}
