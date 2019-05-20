d7a313d11f2c06609fc5d9758cf23073cab73166
Jordan <jordanonuma@outlook.com>
2019-05-20 05:49:30 -1000

    Added frame checking if zombie was previously a crypto kitty.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -20,13 +20,16 @@ contract ZombieFeeding is ZombieFactory {
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
   KittyInterface kittyContract = KittyInterface(ckAddress);
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     //Declares a local Zombie[] named 'myZombie' (which will be a storage pointer) and
     //sets this variable to be equal to index _zombieId in our zombies[] array.
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;   
     uint newDna = (myZombie.dna + _targetDna)/2;
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty")) {
+
+    } //end if(zombie was previously a kitty)
     _createZombie("NoName", newDna);
   } //end function feedAndMultiply()
 
