0b31cbca9a0fc5ba53b2c163bbcc5c8c545e5d92
Jordan <jordanonuma@outlook.com>
2019-05-26 08:41:35 -1000

    1) Set zombies' cooldownTime to 1 day and 2) added the level and cooldownTime parameters to zombies.push(Zombie()).

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -5,6 +5,7 @@ contract ZombieFactory is Ownable {
     event NewZombie(uint zombieId, string name, uint dna);
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
+    uint cooldownTime = 1 days;
 
     struct Zombie {
         string name;
@@ -19,7 +20,7 @@ contract ZombieFactory is Ownable {
     mapping (address => uint) ownerZombieCount;
 
     function _createZombie(string _name, uint _dna) internal {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        uint id = zombies.push(Zombie(_name, _dna, level uint32(now + coolDownTime)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);        
