0e36d11718aead29dc76c82e4108450a34095c1c
Jordan <jordanonuma@outlook.com>
2019-05-29 05:59:08 -1000

    Added frame for a function check, aka modifier, aboveLevel() that makes sure the zombie has a minimum level.

--- ZombieHelper.sol
+++ ZombieHelper.sol
@@ -4,4 +4,8 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+  modifier aboveLevel(uint _level, uint _zombieId) {
+
+   _; 
+  } //end modifier aboveLevel()
 } //end contract ZombieHelper {}
