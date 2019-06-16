36d0f419bc4d0f0bd4dac597018c78ee018b9290
Jordan <jordanonuma@outlook.com>
2019-06-16 08:35:07 -1000

    Added frames for function returning zombie's owner and number of zombies owner has in the ERC721 ownership contract.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -2,5 +2,12 @@ pragma solidity ^0.4.25;
 import "./zombieattack.sol";
 
 contract ZombieOwnership is ZombieAttack {
-    
+  function balanceOf(address _owner) external view returns (uint256) {
+
+  } //end function balanceOf()
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+
+  } //end function ownerOf()
+
 } //end ZombieOwnership {}
