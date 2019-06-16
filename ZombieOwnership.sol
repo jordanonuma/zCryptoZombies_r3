5c7ba3df30f074ad04e16e71a5c87255c68a15a3
Jordan <jordanonuma@outlook.com>
2019-06-16 08:36:29 -1000

    Added calls to functions in zombiefactory.sol that will fufill functions framed.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -3,11 +3,11 @@ import "./zombieattack.sol";
 
 contract ZombieOwnership is ZombieAttack {
   function balanceOf(address _owner) external view returns (uint256) {
-
+      return ownerZombieCount(_owner);
   } //end function balanceOf()
 
   function ownerOf(uint256 _tokenId) external view returns (address) {
-
+      return zombieToOwner(_tokenId);
   } //end function ownerOf()
 
 } //end ZombieOwnership {}
