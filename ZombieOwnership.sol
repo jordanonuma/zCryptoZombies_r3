df5bef2f7e1ec0ab1d735d6a5e6133eaa371f6f3
Jordan <jordanonuma@outlook.com>
2019-06-18 06:29:31 -1000

    Incremented and decremented the zombie receiver and sender, respectively.

--- ZombieOwnership.sol
+++ ZombieOwnership.sol
@@ -11,7 +11,8 @@ contract ZombieOwnership is ZombieAttack {
   } //end function ownerOf()
 
   function _transfer(address _from, address _to, uint256 _tokenId) private {
-      
+    ownerZombieCount[_to]++;
+    ownerZombieCount[_from]--;
   } //end function _transfer()
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
