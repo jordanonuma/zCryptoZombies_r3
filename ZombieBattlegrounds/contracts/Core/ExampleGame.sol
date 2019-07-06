0d9b186729fe0fc86501cb03fecc570c548d4c01
Jordan <jordanonuma@outlook.com>
2019-07-06 11:18:45 -1000

    Added hook (aka solidity function) beforeMatchStart() that core game logic will call before each match.

--- ZombieBattlegrounds/contracts/Core/ExampleGame.sol
+++ ZombieBattlegrounds/contracts/Core/ExampleGame.sol
@@ -4,4 +4,7 @@ import "./ZB/ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
 
+    function beforeMatchStart(bytes serializedGameState) external {
+        
+    } //end function beforeMatchStart()
 } //end contract ExampleGame {}
