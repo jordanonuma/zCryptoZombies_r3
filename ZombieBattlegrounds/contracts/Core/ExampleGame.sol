1621243f6ce9c974051fb641f0f0208d3b67d040
Jordan <jordanonuma@outlook.com>
2019-07-07 08:49:53 -1000

    Added read in for 'gameState' using the 'init' function.

--- ZombieBattlegrounds/contracts/Core/ExampleGame.sol
+++ ZombieBattlegrounds/contracts/Core/ExampleGame.sol
@@ -5,6 +5,7 @@ import "./ZB/ZBGameMode.sol";
 contract ExampleGame is ZBGameMode  {
 
     function beforeMatchStart(bytes serializedGameState) external {
-        GameState memory gameState;        
+        GameState memory gameState;
+        gameState.init(serializedGameState);        
     } //end function beforeMatchStart()
 } //end contract ExampleGame {}
