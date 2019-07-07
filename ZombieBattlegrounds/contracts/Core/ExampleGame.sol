0ae45514249598b4db23727bb4aad1fa24189d97
Jordan <jordanonuma@outlook.com>
2019-07-07 08:48:18 -1000

    Declared a variable called 'gameState' that holds all the states of the game (e.g. player's defense levels, cards in hand, etc.) in memory.

--- ZombieBattlegrounds/contracts/Core/ExampleGame.sol
+++ ZombieBattlegrounds/contracts/Core/ExampleGame.sol
@@ -5,6 +5,6 @@ import "./ZB/ZBGameMode.sol";
 contract ExampleGame is ZBGameMode  {
 
     function beforeMatchStart(bytes serializedGameState) external {
-        
+        GameState memory gameState;        
     } //end function beforeMatchStart()
 } //end contract ExampleGame {}
