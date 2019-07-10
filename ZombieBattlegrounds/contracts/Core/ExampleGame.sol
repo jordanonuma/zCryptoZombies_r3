2f1da76ad047b5373ea6175f81f5a484abb54942
Jordan <jordanonuma@outlook.com>
2019-07-10 06:05:11 -1000

    Created mapping for list of banned cards.

--- ZombieBattlegrounds/contracts/Core/ExampleGame.sol
+++ ZombieBattlegrounds/contracts/Core/ExampleGame.sol
@@ -3,6 +3,7 @@ pragma solidity 0.4.24;
 import "./ZB/ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
+    mapping (string => bool) internal bannedCards;
 
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState;
