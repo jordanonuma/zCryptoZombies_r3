681653ff5481a99c5ebbdaa10e40af1de1ef5e6d
Jordan <jordanonuma@outlook.com>
2019-07-12 06:22:20 -1000

    Revised syntax.

--- ZombieBattlegrounds/contracts/Core/ExampleGame.sol
+++ ZombieBattlegrounds/contracts/Core/ExampleGame.sol
@@ -58,11 +58,11 @@ contract ExampleGame is ZBGameMode  {
     for (uint i = 0; i < gameState.playerStates.length; i++) {
         CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
         uint cardCount = 0;
-        for (uint j = 0; j < gameState.playerStates.cardsInDeck[j].length; j++) {
-            if (isLegalCard(gameSatate.playerStates[i].cardsInDeck[j]) {
-                newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
-                cardCount++;
-            }  //end if ()
-        } //end for (j) {}
+        for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+            if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
+            } //end if ()
+        } //end for (j){}
     } //end for (i){}
 } //end contract ExampleGame {}
